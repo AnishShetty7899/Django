90faf4b054c3c11a6c20ce06272b106408d01f30
AnishShetty7899 <anshshetty7899@gmail.com>
2024-05-02 21:42:49 +0530

    adding path index

--- Project/goApps/urls.py
+++ Project/goApps/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('index', views.index, name='index'),
     path('register', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('profile/', views.profile, name='profile'),
